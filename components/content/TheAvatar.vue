7c57f5f448532ac50c5c8ae40001057ba972a7b2
kvoon <a1176079417@gmail.com>
2024-07-04 22:33:23 +0800

    feat: add avatar shadow

--- components/content/TheAvatar.vue
+++ components/content/TheAvatar.vue
@@ -1,3 +1,3 @@
 <template>
-  <div style="background-image: url('/avatar.jpeg')" href="Kevin Kwong Avatar" size-20 rounded-full border="2" bg="cover center no-repeat" />
+  <div style="background-image: url('/avatar.jpeg')" href="Kevin Kwong Avatar" size-20 rounded-full shadow-lg dark:shadow-bluegray-700 border="2" bg="cover center no-repeat" />
 </template>
