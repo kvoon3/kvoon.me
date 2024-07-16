eb1e5b2af36f19eff5f1efb17ec2e4ede6c173ed
kvoon <a1176079417@gmail.com>
2024-07-16 15:09:15 +0800

    feat(navbar): only display icon when on small screen

--- layouts/home.vue
+++ layouts/home.vue
@@ -11,27 +11,26 @@ const route = useRoute()
 
 <template>
   <div id="home" text-slate-4>
-    <header flex items-center justify-between p4>
-      <section>
-        <NuxtLink flex items-center gap-1 title="Home" to="/">
-          <KIcon animate-fade-in animate-duration-2000 hover="shadow-green" />
-        </NuxtLink>
-      </section>
-      <section flex items-center decoration-none space-x-4>
-        <NuxtLink icon-btn title="Blog" to="/blog">
-          <!-- <i i-iconoir-book inline-block /> -->
-          Blog
-        </NuxtLink>
-        <NuxtLink icon-btn title="Notes" to="/notes">
-          <!-- <i i-iconoir-pen-tablet inline-block />  -->
-          Notes
-        </NuxtLink>
-        <NuxtLink icon-btn title="Projects" to="/projects">
-          <!-- <i i-iconoir-code-brackets inline-block /> -->
-          Projects
-        </NuxtLink>
-        <button i-iconoir-sun-light dark:i-iconoir-half-moon icon-btn @click="() => toggleDark()" />
-      </section>
+    <header flex items-center gap4 p4>
+      <NuxtLink flex items-center gap-1 title="Home" to="/">
+        <KIcon animate-fade-in animate-duration-2000 hover="shadow-green" />
+      </NuxtLink>
+      <div flex-auto />
+      <NuxtLink icon-btn title="Blog" to="/blog">
+        <i i-iconoir-book inline-block sm-hidden />
+        <span lt-sm:hidden>Blog</span>
+      </NuxtLink>
+      <NuxtLink icon-btn title="Notes" to="/notes">
+        <i i-iconoir-pen-tablet inline-block sm-hidden />
+        <span lt-sm:hidden>Notes</span>
+      </NuxtLink>
+      <NuxtLink icon-btn title="Projects" to="/projects">
+        <i i-iconoir-code-brackets inline-block sm-hidden />
+        <span lt-sm:hidden>Projects</span>
+      </NuxtLink>
+      <NuxtLink select-none title="Toggle Dark Scheme" icon-btn @click="() => toggleDark()">
+        <div i-iconoir-sun-light dark:i-iconoir-half-moon />
+      </NuxtLink>
     </header>
     <section px7>
       <slot />
