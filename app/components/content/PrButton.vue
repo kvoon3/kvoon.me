8899c08019b7b6b85dc012268e0d8f979931dbbc
kvoon <a1176079417@gmail.com>
2024-08-23 11:46:04 +0800

    fix: remove default link border

--- app/components/content/PrButton.vue
+++ app/components/content/PrButton.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-  <a href="https://my-pull-requests-kvoon3.nuxt.dev" target="_blank">
-  <button btn inline-flex items-center gap2>
-    <i color-purple i-ph-git-pull-request /> <span>My Recent PR</span>
-  </button>
-</a>
+  <a hover:border-none border-none href="https://my-pull-requests-kvoon3.nuxt.dev" target="_blank">
+    <button  btn inline-flex items-center gap2>
+      <i color-purple i-ph-git-pull-request /> <span>My Recent PR</span>
+    </button>
+  </a>
 </template>
