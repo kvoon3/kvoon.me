48b951e1dc375b05b6cbd18ec8a4a1841ee9ae18
kvoon <a1176079417@gmail.com>
2024-08-23 13:37:59 +0800

    chore: update

--- app/components/content/PrButton.vue
+++ app/components/content/PrButton.vue
@@ -1,10 +1,7 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <a hover:border-none border-none href="https://my-pull-requests-kvoon3.nuxt.dev" target="_blank">
     <button  btn inline-flex items-center gap2>
-      <i color-purple i-ph-git-pull-request /> <span>My Recent PR</span>
+      <i color-purple i-ph-git-pull-request /> <span>Recent PR</span>
     </button>
   </a>
 </template>
