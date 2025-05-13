99088f9598d0c1441f81f9c1c1ff959652c50423
kvoon <a1176079417@gmail.com>
2025-05-13 20:53:33 +0800

    perf: lazy load img

--- pages/photos.vue
+++ pages/photos.vue
@@ -14,13 +14,13 @@ const selectedPhoto = shallowRef<typeof photos[number] | null>(null)
 </script>
 
 <template>
-  <div grid="~ cols-1 sm:cols-2 md:cols-3 lg:cols-4 xl:cols-5 gap-1">
+  <div p2 grid="~ cols-1 sm:cols-2 md:cols-3 lg:cols-4 gap-1">
     <div
       v-for="photo in photos" :key="photo.name"
       aspect-square bg-neutral:10
       @click="selectedPhoto = photo"
     >
-      <NuxtImg :quality="70" :width="720" :src="photo.url" alt="photo" w-full h-full object-cover />
+      <NuxtImg loading="lazy" :quality="70" :width="720" :src="photo.url" alt="photo" w-full h-full object-cover />
     </div>
     <Teleport to="body">
       <div
@@ -31,14 +31,16 @@ const selectedPhoto = shallowRef<typeof photos[number] | null>(null)
       >
         <div absolute z-10 w-full md:max-w70vw>
           <NuxtImg
-            :src="selectedPhoto.url" alt="photo"
+            v-slot="{ isLoaded, src, imgAttrs }" :src="selectedPhoto.url"
+            alt="photo"
             object-cover
             mxa
             md:max-h95vh
-          />
-          <div text-center text-white>
-            {{ selectedPhoto.name }}
-          </div>
+            :custom="true"
+          >
+            <img v-if="isLoaded" v-bind="imgAttrs" :src>
+            <Icon v-else bg-base block name="svg-spinners:bars-fade" mxa size-40 v-bind="imgAttrs" />
+          </NuxtImg>
         </div>
         <div absolute inset-0 backdrop-blur-lg />
       </div>
